a39c582e5becd782d1ce6d4f459be2520f1b6543
ValentG2 <valentin.gregoire@basf.com>
2022-10-17 19:35:47 +0200

    some more settings

--- config.lua
+++ config.lua
@@ -8,6 +8,7 @@ require("configs.scrollbar")
 require("configs.jaq")
 require("configs.onedark")
 require("configs.lualine")
+require("configs.illuminate")
 -- keymaps as last
 require("configs.keymaps")
 
@@ -140,20 +141,20 @@ lvim.builtin.treesitter.highlight.enable = true
 -- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    { command = "black", filetypes = { "python" } },
+    { command = "isort", filetypes = { "python" } },
+    {
+        -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+        command = "prettier",
+        ---@usage arguments to pass to the formatter
+        -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+        extra_args = { "--print-with", "100" },
+        ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+        filetypes = { "typescript", "typescriptreact" },
+    },
+}
 
 -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
@@ -168,6 +169,7 @@ linters.setup {
     },
     {
         command = "codespell",
+        extra_args = { "--extra-words-list", "basf,BASF" }
         ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
         -- filetypes = { "javascript", "python" },
     },
---
+++ lua/configs/illuminate.lua
@@ -0,0 +1,1 @@
+lvim.builtin.illuminate.active = false
