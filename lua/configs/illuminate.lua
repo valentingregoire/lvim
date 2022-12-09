9064b87257d1fd9dc2716bdcfc8490bef0994bc6
ValentG2 <valentin.gregoire@basf.com>
2022-12-09 14:05:48 +0100

    some more stuff

--- lua/configs/illuminate.lua
+++ lua/configs/illuminate.lua
@@ -1,1 +1,14 @@
 lvim.builtin.illuminate.active = true
+lvim.autocommands = {
+    {
+        { "ColorScheme" },
+        {
+            pattern = "*",
+            callback = function()
+                vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#333444", underline = false, bold = false })
+                vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#333444", underline = false, bold = false })
+                vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#333444", underline = false, bold = false })
+            end,
+        },
+    },
+}
